15e05d3d52c563b81ba400591808e30fd20f76af
Pavani-Gunda <pavanigunda23@gmail.com>
2025-01-21 23:08:08 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,73 @@
+#mathematical operations
+print(25+32)
+57
+print(67-35)
+32
+print(658*856)
+563248
+print(756/26)
+29.076923076923077
+print(756//26)
+29
+#using variables
+num1=25
+num2=32
+num3=658*856
+num4=756
+num5=26
+print(num1+num2)
+57
+print(num2-num1)
+7
+print(num3)
+563248
+print(num4/num5)
+29.076923076923077
+
+print(num4//num5)
+29
+
+print(type(num1))
+<class 'int'>
+# strings
+print(type(2.0))
+<class 'float'>
+print(type(2))
+<class 'int'>
+
+>>> print("good_morning")
+good_morning
+
+>>> print("hello everyone good_morning")
+hello everyone good_morning
+>>> print(type("good_morning"))
+<class 'str'>
+>>> print(len("good_morning"))
+12
+>>> good_morning="hello world"
+>>> print("good_morning")
+good_morning
+>>> good_morning
+'hello world'
+>>> print(good_morning)
+hello world
+>>> print(len(good_morning))
+11
+>>> print(type("good_morning"))
+<class 'str'>
+>>> senior= "hello everyone, good morning, glad to meet you, have a nice day"
+>>> senior= "hello everyone, good morning, glad to meet you, have a nice day"
+>>> print(senior)
+hello everyone, good morning, glad to meet you, have a nice day
+>>> print(len(senior))
+63
+>>> print(type(senior))
+<class 'str'>
+>>> print(senior[15])
+ 
+>>> print(senior[7])
+v
+>>> print(senior[7: 18])
+veryone, go
+>>> print(type(senior[7: 25])) #slicing
+<class 'str'>
